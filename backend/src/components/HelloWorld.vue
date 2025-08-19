dd48ce94fa5f6faf0226463969bc34459bd01eb0
Harley Green <harleyrobertgreen@hotmail.com>
2025-08-19 11:17:58 +0100

    Added boilerplate and basic configuring of store and router

--- backend/src/components/HelloWorld.vue
+++ backend/src/components/HelloWorld.vue
@@ -1,10 +1,14 @@
 <script setup>
+import { computed } from 'vue';
+import store from '../store';
+
+const test = computed(() => store.state.test)
 
 </script>
 
 <template>
   <div class="text-gray-800 text-7xl flex justify-center mt-10">
-    Hello World
+    {{ test }}
   </div>
 </template>
 
